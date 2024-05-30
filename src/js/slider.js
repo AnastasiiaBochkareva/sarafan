3a602386728faa6bc2b159caf031f781ecb7e6ae
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2024-05-30 14:38:23 +0400

    install slider

---
+++ src/js/slider.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
