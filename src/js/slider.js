2b440c3d19a1ccc862d601e6aa2f29ad61a344a3
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2024-05-30 15:13:45 +0400

    create slider and cards

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,51 @@
 /* eslint-disable */
-// core version + navigation, pagination modules:
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
+
+import Swiper, {
+  Pagination,
+  Navigation,
+  Mousewheel,
+  FreeMode,
+  Scrollbar,
+} from 'swiper';
+
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import 'swiper/css/mousewheel';
+import 'swiper/css/scrollbar';
+
+const clientsSlider = new Swiper('.clients__slider', {
+  slidesPerView: '3',
+  spaceBetween: 20,
+  navigation: {
+    prevEl: '.main-projects__navigation_prev',
+    nextEl: '.main-projects__navigation_next',
+  },
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  grabCursor: true,
+  freeMode: {
+    enabled: true,
+    sticky: false,
+    momentumBounce: true,
+  },
+  slideToClickedSlide: true,
+  mousewheel: {
+    enabled: true,
+    sensitivity: 9.5,
+  },
+  longSwipesMs: 200,
+  breakpoints: {
+    1440: {
+      slidesPerView: '3',
+      spaceBetween: 20,
+    },
+    767: {
+      slidesPerView: '2',
+    },
+    320: {
+      slidesPerView: '1',
+      spaceBetween: 10,
+    },
+  },
+});
+Swiper.use([Navigation, Pagination, Mousewheel, FreeMode, Scrollbar]);
